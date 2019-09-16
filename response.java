d5110631da51ad7b7f6929c97c0ce4c29baa60b7
Kaylah Tan <kaylah_tan@m018211.aw.org>
2019-09-16 13:01:22 -0700

    hi

--- response.java
+++ response.java
@@ -1,3 +1,4 @@
-Public class response (
-System.out.println(1
-)
+Public class response {
+public static void main(String[] args) {
+System.out.println("1. The companies should invest in more privacy in order for there to be no hackers. 2. They should change their passwords often so they don't get hacked if they do know the passwords. 3. They should do an antivirus software.""
+}
