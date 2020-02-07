4773769d31bbc269ef438ffce367dea612482283
Kaylah Tan <kaylah_tan@m018211.local>
2020-02-06 18:17:44 -0800

    Groundhog.java

---
+++ Groundhog.java
@@ -0,0 +1,25 @@
+class Groundhog {
+  private Boolean cute;
+  private Boolean fuzzy;
+  private Integer springDate;
+  private String color;
+
+  public Groundhog(){
+
+}
+
+public accurate(Boolean m){
+
+}
+
+public popUpOnGroundhogDay(){
+
+}
+
+public springIsEarly(){
+
+}
+  public static void main(String[] args) {
+
+  }
+}
