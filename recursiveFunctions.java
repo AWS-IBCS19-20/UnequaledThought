ac190790a485925f96b964d40550216e01bf99d1
Kaylah Tan <kaylah_tan@m018211.local>
2020-05-05 17:42:07 -0700

    hi

---
+++ recursiveFunctions.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class recursiveFunctions{
+
+public recursiveFunctions() {
+
+}
+
+public Integer bunnyEars(int n){
+  return n;
+  //base case
+  public static void main(String[] args){
+    Recurision recursionTool = new Recursion();
+     recursionTool.calculatebunnyEars()
+    recursiveFunctions.calculatebunnyEars2(0)
+    // 0 + 0
+    recursiveFunctions.calculatebunnyEars2(1)
+    // 0 + 1
+    recursiveFunctions.calculatebunnyEars2(2)
+    // 0 + 2
+  }
+
+
+}
+
+public Integer triangle(int r){
+  return r;
+  public static void main(String[] args){
+    Recursion recursionTool = new Recursion();
+    recursiveFunctions.calculateTriangle(0)
+    // 0 + 0
+    recursiveFunctions.calculateTriangle(1)
+    // 0 + 1
+    recursiveFunctions.calculateTriangle(2)
+    // 0 + 2
+
+  }
+}
+
+public Integer tree(int h){
+  return h;
+}
+
+public static void main(String[] args) {
+
+
+  int inputVal = 4;
+
+  //System.out.println(bunnyEars(inputVal));
+  //System.out.println(triangle(inputVal));
+  //System.out.println(tree(inputVal));
+
+}
+}
